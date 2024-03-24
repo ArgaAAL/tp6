108c68c373d30727c51ddc9ab9cf6b71c3e2983f
ArgaAAL <argalumunon9@gmail.com>
2024-03-24 18:07:27 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223038/Program.cs
+++ tpmodul6_1302223038/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Arga Adolf Lumunon");
+        video.IncreasePlayCount(69); 
+        video.PrintVideoDetails();
+    }
+}
--- tpmodul6_1302223038/obj/Debug/net8.0/tpmodul6_1302223038.AssemblyInfo.cs
+++ tpmodul6_1302223038/obj/Debug/net8.0/tpmodul6_1302223038.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_1302223038")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf66e1fbc3133bf31fc0131045d6e7637f80e19")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_1302223038")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_1302223038")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
